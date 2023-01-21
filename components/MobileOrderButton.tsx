55f081febc27fca7395fffd1f2d7314c7322c5db
ERIK BUINEVICIUS <embuinevicius@gmail.com>
2023-01-20 18:06:26 -0600

    Modified tab names, added Mobile Order button

---
+++ components/MobileOrderButton.tsx
@@ -0,0 +1,57 @@
+import {Button, Pressable, Alert, StyleSheet, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser'
+
+export default function MobileOrderButton() {
+
+    function handleOrderPress() {
+        WebBrowser.openBrowserAsync( 
+        'https://online.skytab.com/9f93befaf4dacdf386b634d4ce057ecf/order-settings'
+        );
+    }
+
+    return (
+        <Pressable style={({pressed}) => [
+            pressed ? styles.buttonPressed : styles.buttonUnpressed,
+        ]}
+        onPress={handleOrderPress}>
+        {({pressed}) => (
+            <Text style={styles.text}>Order Now</Text>
+        )}
+        </Pressable>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    buttonUnpressed: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        borderColor: 'black',
+        borderWidth: 2,
+        backgroundColor: 'rgb(135, 31, 31)',
+    },
+    buttonPressed: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        borderColor: 'black',
+        borderWidth: 2,
+        backgroundColor: 'rgb(82, 3, 3)',
+    },
+    text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+});
+
+
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -14,8 +14,8 @@ import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
+import HomeScreen from '../screens/HomeScreen';
+import MenuScreen from '../screens/MenuScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -58,15 +58,15 @@ function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="Home"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
       }}>
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }: RootTabScreenProps<'Home'>) => ({
+          title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
@@ -85,8 +85,8 @@ function BottomTabNavigator() {
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="Menu"
+        component={MenuScreen}
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
