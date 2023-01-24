6e1826180a2ad705c53715690e4da1b92817ba1c
ebuinevicius <embuinevicius@gmail.com>
2023-01-24 14:35:30 -0600

    Added scrolling to HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import MobileOrderButton from '../components/MobileOrderButton';
+import { usePreventRemoveContext } from '@react-navigation/native';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Ajian</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <MobileOrderButton/>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to Ajian</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <MobileOrderButton/>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -20,6 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
   },
   title: {
     fontSize: 20,
