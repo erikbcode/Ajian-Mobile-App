767ce9f41bb8942461809efad00118e5fae14960
chris000110 <christopher.moorhouse@gmail.com>
2023-02-12 14:46:52 -0600

    Learning how text & importing components works

--- screens/SocialScreen.tsx
+++ screens/SocialScreen.tsx
@@ -1,20 +1,45 @@
 import {useState} from 'react';
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, ScrollView } from 'react-native';
+import MobileOrderButton from '../components/MobileOrderButton';
 import { StyleSheet } from 'react-native';
 
 const SocialScreen = () => {
   return (
-    <View style={styles.container}>
-      {/* Add your component code here */}
+    <ScrollView style = {styles.container}>
+
+    <View>
+      <Text>
+        {'\n'}
+        {'\n'}
+        {'\n'}
+        {'\n'}
+      </Text>
+      <Text style = {styles.words}> Testing</Text>
     </View>
+ 
+    <View>
+      {
+
+        <MobileOrderButton />
+
+      }
+    </View>
+
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(135, 31, 31)'
+    backgroundColor: 'rgb(135, 31, 31)',
+  },
+
+  words: {
+  fontFamily: 'Aboreto',
+  fontSize: 60, 
+  textAlign: 'center',
   },
 });
 
