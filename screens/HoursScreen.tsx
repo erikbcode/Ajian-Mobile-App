cd171bda7c791e86be06f56e28867f45b9c80754
nwcallahan <nwcallahan@crimson.ua.edu>
2023-02-21 05:44:38 -0600

    menu adjustments

--- screens/HoursScreen.tsx
+++ screens/HoursScreen.tsx
@@ -10,11 +10,35 @@ export default function HoursScreen({ navigation }: RootTabScreenProps<'Hours'>)
       <Text style={styles.title}>Hours & Location</Text>
       <Text>Testing branch functionality</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Hours />
       <MobileOrderButton />
     </View>
   );
 }
 
+function Hours() {
+  // firebase call for current hours / specific day hours here
+  const dayHours = [
+    {title: 'Monday', text: '11:00AM - 8:00PM'},
+    {title: 'Tuesday', text: '11:00AM - 8:00PM'},
+    {title: 'Wednesday', text: '11:00AM - 8:00PM'},
+    {title: 'Thursday', text: '11:00AM - 8:00PM'},
+    {title: 'Friday', text: '11:00AM - 8:00PM'},
+    {title: 'Saturday', text: '11:00AM - 8:00PM'},
+    {title: 'Sunday', text: '11:00AM - 8:00PM'},
+  ];
+
+  return (
+    <View style={styles.container}>
+      {dayHours.map((item, index) => (
+        <View key={index} style={styles.day_margin}>
+          <Text style={styles.day_title}>{item.title}</Text>
+          <Text style={styles.day_title}>{item.text}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
 // Function to add two numbers in 
 
 const styles = StyleSheet.create({
@@ -27,8 +51,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  day_margin: {
+    marginBottom: 2,
+  },
+  day_title: {
+    fontSize: 12,
+    fontWeight: 'normal',
+  },
+  hours: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
   separator: {
-    marginVertical: 30,
+    marginVertical: 5,
     height: 1,
     width: '80%',
   },
--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -7,7 +7,75 @@ export default function MenuScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Menu</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {MenuItems('Start with Rice', startWithRice)}
+      {MenuItems('Pick your Wrap', pickYourWrap)}
+      {MenuItems('Pick your Protein', pickYourProtein)}
+      {MenuItems('Make it your Own', makeItYourOwn)}
+      {MenuItems('Finish It', makeItYourOwn)}
+    </View>
+  );
+}
+
+type MenuEntry = {
+  item: string;
+  description: string;
+  price: string;
+}
+
+const startWithRice: MenuEntry[] = [
+  {item: 'White', description: '', price: ''},
+  {item: 'Brown', description: '', price: ''},
+];
+const pickYourWrap: MenuEntry[] = [
+  {item: 'Seaweed', description: 'Traditional wrap with hint of ocean', price: ''},
+  {item: 'Soy', description: 'Soy', price: ''},
+];
+const pickYourProtein: MenuEntry[] = [
+  {item: 'Tuna*', description: '', price: '8.50'},
+  {item: 'Salmon', description: '', price: '8.50'},
+  {item: 'Smoked Salmon*', description: '', price: '8.50'},
+  {item: 'Dynamite Tuna', description: '', price: '8.50'},
+  {item: 'Dynamite Salmon*', description: '', price: '8.50'},
+  {item: 'Grilled Chicken', description: '', price: '7.50'},
+  {item: 'Grilled Steak', description: '', price: '8'},
+  {item: 'Crab Stick', description: '', price: '7.50'},
+  {item: 'Spicy Crab Salad', description: '', price: '7.50'},
+  {item: 'Tempura Shrimp', description: '', price: '8'},
+  {item: 'Poached Shrimp', description: '', price: '8'},
+  {item: 'Roasted Tofu**', description: '', price: '7.50'},
+  {item: 'Veggie**', description: '', price: '7'},
+];
+const makeItYourOwn: MenuEntry[] = [
+  {item: 'Asparagus', description: '', price: ''},
+  {item: 'Avocado', description: '', price: ''},
+  {item: 'Carrot', description: '', price: ''},
+  {item: 'Cream Cheese', description: '', price: ''},
+  {item: 'Cucumber', description: '', price: ''},
+  {item: 'Green Onion', description: '', price: ''},
+  {item: 'Jalapeno Pepper', description: '', price: ''},
+  {item: 'Selection of the Moment', description: '', price: ''},
+  {item: 'Spinach', description: '', price: ''},
+];
+const finishIt: MenuEntry[] = [
+  {item: 'Sauces', description: 'Spicy Mayo, Wasabi Mayo, Sweet Chili Mayo, Sweet Soy (Eel Sauce), Sweet Chili, Sriracha, Sweet Orange Sauce, Spicy Soy', price: ''},
+  {item: 'Toppings', description: 'Sesame Seeds, Tempura Crunch, Sesame Chili Flake, Everything Bagel Spice, Wasabi Pea Crunch, Chopped Peanuts, Fried Onions, Flamin Hot Cheeto Crunch', price: ''},
+  {item: 'Masago', description: '', price: '1.50'},
+  {item: 'Spicy Crab Salad', description: '', price: '2'},
+];
+
+function MenuItems(section_title: string, items: Array<MenuEntry>) {
+  // firebase call for current hours / specific day hours here
+  return (
+    <View style={styles.container}>
+      <View style={styles.small_separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>{section_title}</Text>
+      {items.map((item, index) => (
+        <View key={index} style={styles.small_margin}>
+          <Text style={styles.item_item}>{item.item}</Text>
+          <Text style={styles.item_description}>{item.description}</Text>
+          <Text style={styles.item_price}>{item.price}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -18,13 +86,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  small_margin: {
+    marginBottom: 2,
+  },
   title: {
     fontSize: 20,
+    fontWeight: 'bold',
+  },
+  item_item: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  item_description: {
+    fontSize: 12,
+    fontWeight: 'normal',
+  },
+  item_price: {
+    fontSize: 12,
     fontWeight: 'bold',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  small_separator: {
+    marginVertical: 2,
+    height: 2,
+    width: '60%',
   },
 });
