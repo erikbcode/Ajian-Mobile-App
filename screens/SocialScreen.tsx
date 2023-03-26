237854e964f68b63b61e2b00906978c1240e131d
chris000110 <christopher.moorhouse@gmail.com>
2023-03-26 17:13:32 -0500

    Retrieving from firebase

--- screens/SocialScreen.tsx
+++ screens/SocialScreen.tsx
@@ -1,88 +1,89 @@
 import {useState, useEffect} from 'react';
 import React from 'react';
 import {View, Image, Text, ScrollView,  } from 'react-native';
-import MobileOrderButton from '../components/MobileOrderButton';
 import { StyleSheet } from 'react-native';
-import axios from 'axios';
-import database from '@react-native-firebase/database';
-import { firebase } from '@react-native-firebase/database';
+import { database } from '../firebaseConfig';
+import { ref, get, onValue, push, update, remove, Query} from 'firebase/database';
 
 
-interface IPost {
-  id: number;
-  title: string;
-  body: string;
-  text: string;
-}
+const first = ref(database, `Announcements/1st`);
+const second = ref(database, `Announcements/2nd`);
+const third = ref(database, `Announcements/3rd`);
+const fourth = ref(database, `Announcements/4th`);
+const fifth = ref(database, `Announcements/5th`);
 
-const defaultPosts:IPost[] = [];
-
-const reference = firebase
-  .app()
-  .database('https://ajian-mobile-app-default-rtdb.firebaseio.com/')
-  .ref('/Announcements/Top')
-  .once('value')
-  .then(snapshot => {
-    console.log('User data: ', snapshot.val());
-  });
 
 const SocialScreen = () => {
 
+  var [Announcements, setAnnouncements] = useState<String[]>([]);
+
+  useEffect(() => {
   
- /* const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultPosts);
-  const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
-  const [error, setError]: [string, (error: string) => void] = React.useState("");
+    function pushRows(which: Query) {
 
-  React.useEffect(() => {
-    axios
-    .get<IPost[]>("https://jsonplaceholder.typicode.com/posts")  
-    //.get<IPost[]>("https://api.twitter.com/2/users/863195788167565313/tweets")  
-    .then(response => {
-      setPosts(response.data);
-      setLoading(false);
-    })
-    .catch(ex => {
-      const error =
-      ex.response.status === 404
-        ? "Resource Not found"
-        : "An unexpected error has occurred";
-      setError(error);
-      setLoading(false);
-    });
-  }, []);
+      get(which).then((snapshot) => {
+        Announcements.push(snapshot.val());
+        setAnnouncements(Announcements);
+      })
+    }
+
+    if (Announcements.length < 5) 
+    {
+      pushRows(first);
+      pushRows(second);
+      pushRows(third);
+      pushRows(fourth);
+      pushRows(fifth);
+    }
 
 
-  return(
+  })
 
-    <View style = {styles.container}>
-    <ScrollView>
-      {posts.map((post) => (
+  console.log(Announcements);
 
-          <View key={post.id} style = {styles.tweetBox}>
-              <Text style = {styles.title}>{post.title + '\n'}</Text>
-              <Text style = {styles.text}>{post.body + '\n'}</Text>
-            </View>
-      ))
 
-      }
+  return (
+    <ScrollView style = {styles.container}>
+
+      <View style = {styles.tweetBox}>
+        <Text style = {styles.text}>
+          {Announcements[0]}
+        </Text>
+      </View>
+
+      <View style = {styles.tweetBox}>
+        <Text style = {styles.text}>
+          {Announcements[1]}
+        </Text>
+      </View>
+
+      <View style = {styles.tweetBox}>
+        <Text style = {styles.text}>
+          {Announcements[2]}
+        </Text>
+      </View>
+
+      <View style = {styles.tweetBox}>
+        <Text style = {styles.text}>
+          {Announcements[3]}
+        </Text>
+      </View>
+
+      <View style = {styles.tweetBox}>
+        <Text style = {styles.text}>
+          {Announcements[4]}
+        </Text>
+      </View>
+
+
+
+
+
+
     </ScrollView>
 
-    </View>
-    
-  )*/
 
-  database()
-  .ref('/users/123')
-  .once('value')
-  .then(snapshot => {
-    console.log('User data: ', snapshot.val());
-  });
-
-
-
-
-  
-}
+)}
 
 
 const styles = StyleSheet.create({
@@ -94,7 +95,7 @@ const styles = StyleSheet.create({
 
   text: {
   fontFamily: 'Aboreto',
-  fontSize: 12, 
+  fontSize: 20, 
   textAlign: 'center',
   backgroundColor: 'rgb(254, 251, 234)'
   },
@@ -109,7 +110,7 @@ const styles = StyleSheet.create({
 
   tweetBox: {
     borderColor: 'rgb(135, 31, 31)', 
-    borderWidth: 20
+    borderWidth: 25
   }
 });
 
