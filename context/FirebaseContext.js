73c4363877b49d1bf633a2b40e2502622b0436e7
ERIK BUINEVICIUS <embuinevicius@gmail.com>
2023-03-27 22:23:04 -0500

    Improved commenting, removed unused catches / variables

--- context/FirebaseContext.js
+++ context/FirebaseContext.js
@@ -6,96 +6,80 @@ import { auth, database } from '../firebaseConfig';
 
 const FirebaseContext = React.createContext();
 
+// Wrapper function for useContext() to make it easier to use in other files
 export function useFirebase() {
     return useContext(FirebaseContext);
 }
 
 export function FirebaseProvider({children}) {
     const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState(null); // Contains auth user object
+    const [user, setUser] = useState(null); // Firebase auth user object
     const [userData, setUserData] = useState(null); // Contains data for user in object format {fullName: string, rewardsPoints: int, hasSignUpReward: boolean, userEmail: string, phoneNumber: string}
-    const [hoursData, setHoursData] = useState(null);
 
     // Function to sign a user up. Calls firebase method to create auth user, and then uses this new user to update the database and local state data.
     async function signUp(email, password, name, phoneNumber) {
 
         email = email.toLowerCase();
 
-        try {
-            if (!phoneNumber) {
-                Alert.alert('no phone', 'No phone number');
+        const phoneRef = ref(database, `phoneNumbers/${phoneNumber}`);
+
+        get(phoneRef).then((snapshot) => {
+            // Phone number already in database, so cancel sign up
+            if (snapshot.exists()) {
+                console.log(snapshot.val())
+                Alert.alert('Sign Up Failed', 'Please enter a valid 10-digit phone number that is not in use')
                 return;
-            }
-
-            const phoneRef = ref(database, `phoneNumbers/${phoneNumber}`);
-
-            get(phoneRef).then((snapshot) => {
-                if (snapshot.exists()) {
-                    console.log(snapshot.val())
-                    Alert.alert('Phone Number In Use', 'Please enter a valid 10-digit phone number that is not in use.')
-                    return;
-                } else {
-                    console.log(snapshot.val())
-                    setLoading(true)
-                    // Create a new user with email and password
-                    createUserWithEmailAndPassword(auth, email, password)
-                        .then((userCredential) => {
-                            const newUser = userCredential.user;
-                            updateProfile(newUser, {displayName: name, phoneNumber: phoneNumber}).then(() => {
-                                setUserData({fullName: name, rewardsPoints: 0, hasSignUpReward: true, userEmail: email, phoneNumber: phoneNumber})
-                                const userRef = ref(database, `users/${newUser.uid}`)
-                                set(userRef, {
-                                    fullName: name,
-                                    rewardsPoints: 0,
-                                    hasSignUpReward: true,
-                                    userEmail: email,
-                                    phoneNumber: phoneNumber
-                                }).then(() => {
-                                    const phoneUserRef = ref(database, `phoneNumbers/${phoneNumber}`)
-                                    set(phoneUserRef, { fullName: name, userEmail: email, userId: newUser.uid}).then(() => {
-                                        return(newUser);
-                                    })
-                                })
+            } else {
+                // Create a new user with email and password
+                createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                    const newUser = userCredential.user;
+                    // Update auth profile information for name and phone
+                    updateProfile(newUser, {displayName: name, phoneNumber: phoneNumber}).then(() => {
+                        // Set user data in local state
+                        setUserData({fullName: name, rewardsPoints: 0, hasSignUpReward: true, userEmail: email, phoneNumber: phoneNumber})
+                        // Set user data in users database
+                        const userRef = ref(database, `users/${newUser.uid}`)
+                        set(userRef, {
+                            fullName: name,
+                            rewardsPoints: 0,
+                            hasSignUpReward: true,
+                            userEmail: email,
+                            phoneNumber: phoneNumber
+                        }).then(() => {
+                            // Set user data in phoneNumbers database
+                            const phoneUserRef = ref(database, `phoneNumbers/${phoneNumber}`)
+                            set(phoneUserRef, { fullName: name, userEmail: email, userId: newUser.uid}).then(() => {
+                                return(newUser);
                             })
                         })
-                        .catch((error) => {
-                            if (error.code == 'auth/email-already-in-use') {
-                                Alert.alert('Error', 'The specified email is already associated with an account.');
-                            }
-                            console.log(error);
-                        });
-                }
-            })            
-        } catch (error) {
-            console.error('failed, ' +  error);
-        }
-    }
-
-    // Logs a user into the platform. Calls firebase method to sign in, and then accesses the database to update local data. 
-    function logIn(email, password) {
-        return signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const loggedUser = userCredential.user;
-                setUser(loggedUser);
-                const userDataRef = ref(database);
-                get(child(userDataRef, `users/${loggedUser.uid}`)).then((snapshot) => {
-                    if (snapshot.exists()) {
-                        setUserData(snapshot.val());
-                    } else {
-                        console.log('No data available')
-                    }
-                }).catch((error) => {
-                    console.log(error)
+                    })
                 })
-            });
+            }
+        })            
     }
 
-    // Logs a user out by calling the signOut() firebase method
+    // Function to log a user into the platform. Calls Firebase auth method to sign in, and then accesses the database to update client-side state data.
+    function logIn(email, password) {
+        return signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            const loggedUser = userCredential.user; // Grab user credentials of the user that was just signed in
+            setUser(loggedUser); // Set the current user state to the logged in user
+            const userDataRef = ref(database);
+            get(child(userDataRef, `users/${loggedUser.uid}`)).then((snapshot) => {
+                if (snapshot.exists()) {
+                    setUserData(snapshot.val()); // Set current state data to the user's existing data in realtime database if it exists
+                } else {
+                    console.log('No user data available.')
+                }
+            })
+        });
+    }
+
+    // Logs a user out by calling the appropriate Firebase auth method
     function logOut() {
         return signOut(auth);
     }
 
-    // Sends a password reset email associated with the email 
+    // Sends a password reset email to the specified email address
     function resetPassword(email) {
         return sendPasswordResetEmail(auth, email);
     }
@@ -104,32 +88,37 @@ export function FirebaseProvider({children}) {
     function redeemReward() {
         // Update database to reflect that the reward has been used
         if (user) {
+            // Update the reward status in realtime database
             update(ref(database, `users/${user.uid}`), {
                 hasSignUpReward: false
             });
+            // Update the reward status in local state
             setUserData({
                 ...userData, // spready operator to copy all existing userData fields
-                hasSignUpReward: false});
-        };
+                hasSignUpReward: false
+            });
+        }
     }
 
     // Effectively deletes a user's account by removing their data in the users database, then their authorization data, and then their data in the phoneNumbers database
     function deleteAccount() {
+        // First remove data from the phoneNumbers database
         const phoneRef = ref(database, `phoneNumbers/${userData.phoneNumber}`);
         remove(phoneRef).then(() => {
             console.log('Phone number deleted successfully.');
+            // Then remove data from the users database
             const userRef = ref(database, `users/${user.uid}`)
             remove(userRef).then(() => {
                 console.log('User data deleted successfully');
+                // Finally, remove the user's authorization data
                 user.delete().then(() => {
                     console.log('User account deleted successfully.');
                 })
             })
-        }).catch((error) => {
-            console.log(error);
-        });
+        })
     }
 
+    // Refresh local state for user data using info passed in
     function refreshUserData(fullName, rewardsPoints, hasSignUpReward, userEmail, phoneNumber) {
         setUserData({fullName: fullName, rewardsPoints: rewardsPoints, hasSignUpReward: hasSignUpReward, userEmail: userEmail, phoneNumber: phoneNumber});
     }
@@ -151,7 +140,7 @@ export function FirebaseProvider({children}) {
     }, []);
 
     return (
-        <FirebaseContext.Provider value={{loading, user, setUser, userData, setUserData, hoursData, signUp, logIn, logOut, resetPassword, redeemReward, deleteAccount, refreshUserData}}>
+        <FirebaseContext.Provider value={{loading, user, setUser, userData, setUserData, signUp, logIn, logOut, resetPassword, redeemReward, deleteAccount, refreshUserData}}>
             {children}
         </FirebaseContext.Provider>
     )
