10fe42fc821fa14e2e34fcc81d57da4054617c13
ERIK BUINEVICIUS <embuinevicius@gmail.com>
2023-03-30 15:23:36 -0500

    Removed deprecated values

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,16 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          "moduleName": "@env",
-          "path": ".env",
-          "blacklist": null,
-          "whitelist": null,
-          "safe": false,
-          "allowUndefined": true
-        }
-      ]
+      ["module:react-native-dotenv", {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+        "blocklist": null,
+        "allowlist": null,
+        "safe": false,
+        "allowUndefined": true,
+        "verbose": false
+      }]
     ]
   };
 };
