f61782f3c160c0c5ec1438451b664442c5402f14
Emily Lucas <ealucas1@crimson.ua.edu>
2023-04-13 13:19:59 -0500

    admin setup

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,14 +11,14 @@ import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBVaDg6hVkbYrTgX81ybB9kh6KIfppUcWY",
+  authDomain: "ajian-mobile-app.firebaseapp.com",
+  databaseURL: "https://ajian-mobile-app-default-rtdb.firebaseio.com",
+  projectId: "ajian-mobile-app",
+  storageBucket: "ajian-mobile-app.appspot.com",
+  messagingSenderId: "837255087515",
+  appId: "1:837255087515:web:6bb081c2c64aaea2224d3e",
+  measurementId: "G-PJT3RNSX6C"
 };
 
 const app = initializeApp(firebaseConfig);
