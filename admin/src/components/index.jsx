761573bee3ce11ac5f95f4f874f785473825f2ee
Emily Lucas <ealucas1@crimson.ua.edu>
2023-04-19 20:37:56 -0500

    add pages

---
+++ admin/src/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as ThemeSettings } from './ThemeSettings';
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as UserProfile } from './UserProfile';
---
+++ admin/src/pages/Hours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hours = () => {
+  return (
+    <div>Hours</div>
+  )
+}
+
+export default Hours
