1ae6ecfa1b8eba2daeb1ac51f6e38f823aa6c69d
Emily Lucas <ealucas1@crimson.ua.edu>
2023-04-25 14:52:25 -0500

    Announcements update

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
-
+import React, {useState, useEffect} from 'react';
+import { database } from "../firebaseConfig";
 import { Header } from '../components';
-import { EditorData } from '../data/data';
+import { ref, set, get, onValue, push, update, remove, Query} from 'firebase/database';
+import 'firebase/database';
 
-const Announcements = () => (
-  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+const announcementsRef = ref(database, `Announcements`);
+
+const Announcements = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  var [Announcements, setAnnouncements] = useState([]);
+
+  //Maintain variable
+  useEffect(() => {
+
+    //Retrieve the data from firebase
+    get (announcementsRef).then((snapshot) => {
+      setAnnouncements(snapshot.val());
+    })
+
+  }, [Announcements])
+  const keys = Object.keys(Announcements);
+
+  //Convert the strings into numbers for use in indexing
+    const lookup = keys.map((key) => Number(key));
+    const lastKey = lookup.length;
+
+  function handleAddAnnouncement() {
+    update(announcementsRef, {
+      [lastKey]: content,
+    });
+    setContent("");
+  }
+
+  return(
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <Header title="Announcements" />
-    <RichTextEditorComponent>
-      <EditorData />
-      <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
-    </RichTextEditorComponent>
-  </div>
-);
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      ></textarea>
+      <button 
+        type = "button"
+        className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
+        onClick={handleAddAnnouncement}>Add Announcement</button>
+      <ul>
+        {/* Display a list of announcements */}
+      </ul>
+      
+    </div>
+  );
+  
+}
 export default Announcements;
