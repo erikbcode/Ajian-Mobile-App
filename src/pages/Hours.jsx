7618b05e3b9607ca886e1036e03422d281d7189a
Emily Lucas <ealucas1@crimson.ua.edu>
2023-04-27 22:37:58 -0500

    hours page update

--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
+import { database } from "../firebaseConfig";
 import { Header } from '../components';
+import { ref, set, get, onValue, push, update, remove, Query} from 'firebase/database';
+import 'firebase/database';
 
-const Hours = () => (
+const hoursRef = ref(database, `Hours`);
+
+const Hours = () => {
+  const [content, setContent] = useState("");
+  var [hours, setHours] = useState([]);
+
+  return (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <Header title="Hours" />
+    <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      ></textarea>
   </div>
-)
+  );
+}
 
 export default Hours
